7b8b6217eb5fc1687ad86a4e81f455b51398fea3
yijie37 <yijiehere@163.com>
2024-09-30 18:43:40 +0800

    add button, add game infomation

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   const [userToken, setUserToken] = useState<string>("0");
   const [firstImage, setFirstImage] = useState<string>('');
   const [thirdValue, setThirdValue] = useState<number>(3);
-  
+
   useEffect(() => {
     const initializeApp = () => {
       if (WebApp.initDataUnsafe.user) {
@@ -158,8 +158,6 @@ export default function Home() {
     return userRegistrations.length - 1; // Return 1 if userId is smaller than all IDs in the array
   }
 
-  
-
   function getMagnitude(n: number) {
     return n === 0 ? 0 : Math.floor(Math.log(n) / Math.log(10)) + 1;
   }
@@ -188,10 +186,33 @@ export default function Home() {
     });
   }
 
+  function handleFollowTwitter() {
+    const utils = initUtils();
+    utils.openLink('https://x.com/TheVirusGames', {
+      tryInstantView: true,
+      tryBrowser: true
+    });
+  }
+
   return (
     <div className="bg-black min-h-screen px-4 sm:px-8 py-6 sm:py-10 flex flex-col justify-between max-w-md mx-auto">
       <div className="flex flex-col gap-4 sm:gap-6">
-        <img className='w-32 h-28 mx-auto' src="/images/final.webp" alt="" />
+        <div className="flex items-center justify-center relative">
+          <img className='w-32 h-28' src="/images/final.webp" alt="" />
+          <PopoverCom 
+            headImg={firstImage}
+            firstLine="How to Play"
+            secondLine="By sharing invitations to spread the virus, you increase the biohazard progress. When the progress reaches 99%, various mutant viruses start to emerge. Upon reaching 100% progress, you can obtain a large amount of viruses or 1 ETH."
+            hideHeaderImg={true}
+            icon={
+              <img 
+                src="/images/info-icon.svg" 
+                alt="Info" 
+                className="absolute right-0 w-6 h-6 cursor-pointer"
+              />
+            }
+          />
+        </div>
         
         {/* Referral Reward group */}
         {btcToTake && <div className="border-2 border-gray-700 rounded-lg p-4 relative">
@@ -245,6 +266,9 @@ export default function Home() {
       </div>
       
       <div className="mt-4 sm:mt-6 space-y-2 sm:space-y-4">
+        <div className="bg-lime-500 text-black text-center h-10 leading-10 rounded-lg" onClick={handleFollowTwitter}>
+          Follow Twitter
+        </div>
         <div className="bg-lime-500 text-black text-center h-10 leading-10 rounded-lg" onClick={handleChannel}>
           Join Telegram Channel
         </div>
